77c41a8439ab7d12e8a3944714076b47d308a71d
Johnsavadkuhi <physitime@gmail.com>
2025-10-21 15:53:52 +0330

    init

---
+++ src/pages/login.jsx
@@ -0,0 +1,25 @@
+import { useTheme } from "../theme/ThemeContext";
+
+export default function Login() {
+
+    const { state, dispatch } = useTheme()
+
+    console.log("state : ", state)
+
+
+    return (
+        <div
+          style={{
+            background: state.theme === "light" ? "#fff" : "#222",
+            color: state.theme === "light" ? "#000" : "#fff",
+            fontSize: state.fontSize,
+            padding: "1rem",
+          }}
+        >
+            <button onClick={() => dispatch({ type: "TOGGLE_THEME" })}>
+                Toggle Theme
+            </button>
+            
+        </div>
+    );
+}
---
+++ src/theme/ThemeContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useReducer } from "react";
+
+
+export const ThemeContext = createContext();
+
+const initialState = { theme: "light", fontSize: 16 }
+
+function reducer(state, action) {
+
+    switch (action.type) {
+        case "TOGGLE_THEME":
+            return { ...state, theme: state.theme === "light" ? "dark" : "light" }
+        case "INCREASE_FONT":
+            return { ...state, fontSize: state.fontSize + 2 }
+        case "DECREASE_FONT":
+            return { ...state, fontSize: Math.max(10, state.fontSize - 2) }
+        default:
+            return state;
+
+    }
+}
+
+function ThemeProvider({ children }) {
+ 
+    const [state , dispatch ] = useReducer(reducer , initialState)
+
+    const value = {state , dispatch } 
+    return (
+        <ThemeContext value={value}>
+            {children}
+        </ThemeContext>
+    )
+
+
+}
+
+
+export function useTheme() {
+    const context = useContext(ThemeContext)
+    if (!context) throw new Error("useTheme must be inside ThemeProvider ");
+    return context;
+
+}
+
+export default ThemeProvider 
