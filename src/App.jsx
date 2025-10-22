31bd19dfc3e1f8a9c7f69c18578d44bf66637314
Johnsavadkuhi <physitime@gmail.com>
2025-10-22 13:46:02 +0330

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,26 @@
-import { Activity, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import './App.css'
-import Login from './pages/login'
 import ThemeProvider from './theme/ThemeContext'
+import Theme from './pages/Theme'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <ThemeProvider>
-      <div>
-        
-        <Activity mode={count===2 ? "hidden":"visible"}>
 
-        
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        </Activity>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() =>
+  setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Login/>
 
-  </ThemeProvider>
+      </div>
+
+      <Theme />
+
+    </ThemeProvider>
   )
 }
-
 export default App
