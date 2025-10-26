fdd7d27edaccc10aa7e404d45e6cb9176dba85c1
Johnsavadkuhi <physitime@gmail.com>
2025-10-26 14:59:47 +0330

    add

--- src/pages/Name.jsx
+++ src/pages/Name.jsx
@@ -1,17 +1,18 @@
+
+
 import { useState, useEffect } from "react";
 
-export default function Name() {
+export default function Count() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // ❌ stale closure: `count` is always 0
       console.log(count);
       setCount(count + 1);
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [count]); // Empty dependency array!
+  }, []); 
   
-  return <div>{count/60}</div>;
+  return <div>{count}</div>;
 }
